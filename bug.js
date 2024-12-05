0df2f90b9dbd4ef5b429fc90e44a999ff7614fce
BugHunter-X <askzenai@gmail.com>
2024-12-05 10:26:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+While using Expo CLI, I encountered an issue where the development server would randomly crash without providing any error messages in the terminal. This would happen intermittently, making debugging and development incredibly difficult. The project was relatively simple, using React Navigation and a few other common Expo modules.
